7a7dd15ae6dd7d83c3b8a657404ce677a9e70cbe
tlips <Thomas.Lips@UGent.be>
2020-09-27 15:33:38 +0200

    complete helpers for load,split,write & split dataset

--- helpers.py
+++ helpers.py
@@ -1,16 +1,56 @@
 import numpy as np
 
-def load_csv(dataset_path, delimiter = ",", usecols = None):
+def load_csv(dataset_path, delimiter = ",", usecols = None, converters=None):
     '''
     creates a structured np.array from the dataset where each datapoint will be a single tuple in the array, consisting of multiple named datatypes
     '''
     dtype = None # figure out the types by first trying Bool, the Int, then Float
     names = True # use first row as column names
-    convertfunc = lambda x: 0 if b'b' in x else 1 # convertfucntion for Prediction column to 0 if bg, and 1 if signal
+    
     if usecols is not None:
-        data = np.genfromtxt(dataset_path, dtype=dtype, delimiter=delimiter, names=names, usecols= usecols,converters={"Prediction": convertfunc} )
+        data = np.genfromtxt(dataset_path, dtype=dtype, delimiter=delimiter, names=names, usecols= usecols,converters=converters )
     else: 
-        data = np.genfromtxt(dataset_path, dtype=dtype, delimiter=delimiter, names=names,converters={"Prediction": convertfunc})
+        data = np.genfromtxt(dataset_path, dtype=dtype, delimiter=delimiter, names=names,converters=converters)
 
     return data
     
+def split_dataset(dataset, test_ratio = 0.1):
+    '''
+    expects as input a structured npdarray, with the first column being unique (an identifier)
+    splits the dataset in train and testset according to the test_ratio 
+
+    '''
+    np.random.seed(2020) # fix seed for reproducability 
+    shuffled_indices = np.random.permutation(len(dataset))
+    test_set_size = int(len(dataset)*test_ratio)
+    test_indices = shuffled_indices[:test_set_size]
+    train_indices =shuffled_indices[test_set_size:]
+
+
+    return dataset[train_indices], dataset[test_indices]
+
+
+def write_csv(data, filename):
+    # create format array
+    fmt = []
+    for name in data.dtype.names:
+        t = str(data.dtype[name])[0]
+        if t is "i":
+            fmt.append("%d")
+        if t is "f":
+            fmt.append("%3.3f")
+
+     # create header
+    header = ','.join(data.dtype.names)
+
+    np.savetxt(filename, data, delimiter=',', header=header, fmt=fmt, comments="")
+
+
+if __name__ == "__main__":
+
+    convertfunc = lambda x: 0 if b'b' in x else 1 # convertfucntion for Prediction column to 0 if bg, and 1 if signal
+    converters={"Prediction": convertfunc}
+    dataset = load_csv("dataset/train.csv", converters=converters)
+    train,test = split_dataset(dataset)
+    write_csv(train, "dataset/trainset.csv")
+    write_csv(test,"dataset/testset.csv")
--- test/test_helpers.py
+++ test/test_helpers.py
@@ -1,19 +1,37 @@
 import unittest 
 
-from helpers import *
+from helpers import load_csv, split_dataset, write_csv
 
 class TestHelpers(unittest.TestCase):
     def setUp(self):
         self.path = "test/dummy_data.csv"
 
     def test_load_data(self):
-        data = load_csv(self.path)
+        convertfunc = lambda x: 0 if b'b' in x else 1 # convertfucntion for Prediction column to 0 if bg, and 1 if signal
+        converters={"Prediction": convertfunc}
+        data = load_csv(self.path, converters=converters)
+
         self.assertEqual(data[0]['Id'], data[0][0]) #test dtype naming 
         self.assertEqual(data[0]["Prediction"],1) # test conversion of Prediction strings
-        self.assertEqual(data.shape, (9,)) 
-        print(data)
+        self.assertEqual(data.shape, (10,)) 
+        #print(data)
+        #print(data.dtype)
 
 
-        
+    def test_split_data(self):
+        data = load_csv(self.path)
+
+        trainset,testset = split_dataset(data, test_ratio=0.4)
+        self.assertEqual(trainset.shape, (6,))
+        self.assertEqual(testset.shape, (4,))
+
+        #print(trainset)
+        #print(testset)
+
+    def test_write_csv(self):
+        convertfunc = lambda x: 0 if b'b' in x else 1 # convertfucntion for Prediction column to 0 if bg, and 1 if signal
+        converters={"Prediction": convertfunc}
+        data = load_csv(self.path, converters=converters)
+        write_csv(data,"test/test_write.csv")
 
 
