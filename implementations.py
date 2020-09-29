a83ae35dbb2b2e3558d2c495f93e26040722c8da
tlips <Thomas.Lips@UGent.be>
2020-09-29 13:47:49 +0200

    add template of function as example

---
+++ implementations.py
@@ -0,0 +1,19 @@
+def least_squares_GD(y, tx, initial_w, max_iters, gamma):
+    """ performs linear regression using Gradient Descent
+
+    :param y: labels
+    :type y: numpy array
+    :param tx: extended (contains bias column) feature matrix, where each row is a datapoint and each column a feature
+    :type tx: numpy 2D array
+    :param initial_w: initial values for the regression weight vector
+    :type initial_w: numpy array
+    :param max_iters: number of max iterations for Gradient Descent
+    :type max_iters: int
+    :param gamma: Gradient Descent step size
+    :type gamma: float
+
+    :return: a tuple containing (weight vector, loss value)
+    :rtype: (numpy array, float)
+    """
+
+    raise NotImplementedError
