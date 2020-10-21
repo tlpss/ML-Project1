fc58d7e8ea088287bb2c5fed36048b5f997a8011
tlips <Thomas.Lips@UGent.be>
2020-10-21 17:48:28 +0200

    confusion matrix test + implementation small fix for log_reg test

--- test/test_implementations.py
+++ test/test_implementations.py
@@ -58,20 +58,30 @@ class TestImplementations(unittest.TestCase):
         self.assertTrue(np.dot(diff,diff.T) < 0.01) # should not be too far from "exact weight vector"
         
     def test_reg_logistic_regression_extremes(self):
-        print(self.y_ridge)
-        w,loss = reg_logistic_regression(self.y_ridge, self.tX,0.2,np.array([0,0,0]), 50, 0.1)
+        #print(self.y_ridge)
+        w,loss = reg_logistic_regression(self.y_ridge, self.tX,0.2,np.array([0,0,0]), 100, 0.01)
         y_ridge = np.dot(self.tX,w)
-        print("Loss", loss)
+        #print("Loss", loss)
         sig = 1/(1 + np.exp(-y_ridge))
         f = lambda x : float(0) if x <0.5 else float(1)
         y_ridge = np.array([ f(x) for x in sig])
-        print(y_ridge == self.y_ridge)
+        #print(y_ridge == self.y_ridge)
         diff = y_ridge == self.y_ridge
         mean = np.mean(diff*1)
-        self.assertTrue(mean > 0.97) # should not be too far from "exact weight vector"
-        
-        #w,loss = reg_logistic_regression(self.y_ridge, self.tX, 10000000,np.array([0,0,0]), 50, 0.1)
-        #self.assertTrue(np.dot(w,w.T) < 0.001)
+        self.assertTrue(mean > 0.97) # accuracy should be very high
+
+    def test_confusion_matrix(self):
+        y = np.array([1,1,1,0,0,0])
+        y_hat = np.array([1,1,1,1,0,0])
+
+        c = confusion_matrix(y,y_hat)
+        #print(c)
+        self.assertAlmostEqual(c[0,0],2/3)
+        self.assertAlmostEqual(c[1,1],3/3)
+        self.assertAlmostEqual(c[0,1],1/3)
+        self.assertAlmostEqual(c[1,0],0/3)
+
+ 
         
         
 
